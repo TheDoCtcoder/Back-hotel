4f34258254a4c247075cad15bbee2d5ca52f8549
Lardo <mlvproconcept@gmail.com>
2022-09-06 14:11:36 +0200

    Feature Root RoomRouter

---
+++ Root/room-router.js
@@ -0,0 +1,37 @@
+//? ///////////////////////// MISE En PLACE ROOTING Room (Router) /////////////////////////
+
+
+
+
+//todo Initialising EXPRESS
+//todo _______________________________
+const roomRouter = require('express').Router()
+const roomController = require('../Controllers/room-controller')
+
+
+
+
+//todo ROOT configuration
+//todo ________________________________
+// .get    = Recuperation / Afficher
+// .post   = Ajout
+// .put    = Modification
+// .delete = Supression
+
+
+//* ------methode long (repetition)--------------
+
+roomRouter.get('/', roomController.getALL)
+
+roomRouter.get('/:id', idValidator(), roomController.getById)
+
+roomRouter.put('/:id', authentication(['Admin']), idValidator(), roomController.update)
+
+roomRouter.post('/', authentication(['Admin']), roomController.creat)
+
+roomRouter.delete('/:id', idValidator(), authentication(['Admin']), roomController.delete)
+
+
+//todo Exported ROOT
+//todo ________________________________
+module.exports = roomRouter
